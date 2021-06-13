345faee6f3fa52aa577850eee9ebaf579e22e980
mlima3696 <mjuniorlima3@gmail.com>
2021-06-12 22:25:14 -0300

    Projeto Criado

---
+++ src/main/java/br/com/mlima/projetomvc/ProjetomvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.mlima.projetomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomvcApplication.class, args);
+	}
+
+}

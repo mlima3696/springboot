7315d194cd769a64966b24bf078d4ea0bc7a63aa
mlima3696 <mjuniorlima3@gmail.com>
2021-06-17 00:42:49 -0300

    Criando operação de instanciação

--- src/main/java/br/com/mlima/projetomvc/ProjetomvcApplication.java
+++ src/main/java/br/com/mlima/projetomvc/ProjetomvcApplication.java
@@ -1,13 +1,33 @@
 package br.com.mlima.projetomvc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetomvcApplication {
+import br.com.mlima.projetomvc.domain.Categoria;
+import br.com.mlima.projetomvc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetomvcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomvcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); //Criacao automatica de listas
 	}
 
 }
